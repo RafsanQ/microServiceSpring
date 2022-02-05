4028d6f067d30f276332aafcb0e1827c491c1faa
Rafsan <rafsan.quayes@gmail.com>
2022-02-05 22:38:18 +0600

    Controller for products done

--- product-service/src/main/java/com/example/productservice/controllers/ProductController.java
+++ product-service/src/main/java/com/example/productservice/controllers/ProductController.java
@@ -1,5 +1,23 @@
 package com.example.productservice.controllers;
 
-public class ProductController {
+import com.example.productservice.entities.Product;
+import com.example.productservice.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+    @Autowired
+    private ProductService productService;
+
+    @PostMapping("/")
+    public Product saveProduct(@RequestBody Product product){
+        return productService.saveProduct(product);
+    }
+
+    @GetMapping("/{id}")
+    public Product findProductById(@PathVariable("id") String id){
+        return productService.findProductById(id);
+    }
 }
