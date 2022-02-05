53fb8a39f6eba7d623ed334f21033b5ab9e6d1ca
Rafsan <rafsan.quayes@gmail.com>
2022-02-05 22:33:38 +0600

    Made the Service and Repository classes

--- product-service/src/main/java/com/example/productservice/repositories/ProductRepository.java
+++ product-service/src/main/java/com/example/productservice/repositories/ProductRepository.java
@@ -1,4 +1,11 @@
 package com.example.productservice.repositories;
 
-public interface ProductRepository {
+import com.example.productservice.entities.Product;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends MongoRepository<Product,String> {
+
+    Product findProductById(String id);
 }
